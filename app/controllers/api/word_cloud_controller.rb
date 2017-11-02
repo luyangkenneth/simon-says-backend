40f74c97c72c1176413005e88e5399bfc2f3678a
Kenneth Lu <lu.yang.kenneth@gmail.com>
2017-11-02 16:52:01 +0800

    Reset routes

--- app/controllers/api/word_cloud_controller.rb
+++ app/controllers/api/word_cloud_controller.rb
@@ -1,18 +1,11 @@
 class Api::WordCloudController < ApplicationController
   # q5
   def index
-    json = []
-
-    Publication.where("this.venue.toLowerCase() == '#{venue}'").each do |publication|
-      json << publication.title
-    end
+    json = {
+      title: 'hello',
+      message: 'world'
+    }
 
     json_response(json)
-  end
-
-  private
-
-  def venue
-    params[:venue].downcase || 'arxiv'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    # q1
-    # /api/authors_by_num_publications?top=10&venue=arXiv
-    get 'authors_by_num_publications', to: 'authors#index'
-
-    # q2
-    # /api/publications_by_num_citations?top=5&venue=arXiv
-    get 'publications_by_num_citations', to: 'publications#index'
-
-    # q3
-    # /api/num_publications_by_year?venue=ICSE
-    get 'num_publications_by_year', to: 'publications_trend#index'
-
-    # q4
-    # /api/citation_web?title=Low-density parity check codes over GF(q)
-    get 'citation_web', to: 'citation_web#index'
-
-    # q5
-    # /api/word_cloud?venue=arXiv
     get 'word_cloud', to: 'word_cloud#index'
   end
 end
