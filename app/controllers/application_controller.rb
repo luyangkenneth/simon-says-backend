c1de7b2ae9517f84b1644a370ad3dec3c01a458d
Kenneth Lu <lu.yang.kenneth@gmail.com>
2017-11-09 14:43:49 +0800

    Handle errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include Response
+
+  rescue_from StandardError do |error|
+    render json: { error: error.message }, status: :bad_request
+  end
 end
